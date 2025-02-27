c2229d8d08496def23371d3ce25a3df3fd09d947
Achu-kappithan <achukappithan4236@gmail.com>
2025-02-27 14:52:56 +0530

    server  starts and database connected

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
-import mongoose from "mongoose"
+
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import connectdb from "./config/db";
+
+
+dotenv.config()
+const app =express()
+const Port = process.env.PORT || 3008
+
+app.use(cors())
+app.use(bodyParser.json())
+
+connectdb()
+
+
+app.get("/",(req,res)=>{
+    res.send("Student management system is running")
+})
+
+app.listen(Port,()=>console.log(`Server startson http://localhost:${Port}`))
